4381f0c17ae592c255242f073b283d6144df3a88
Jimmy Kuruvilla <jimmyjkatwork@gmail.com>
2024-09-07 13:41:58 -0500

    Move summary to json files

--- src/debug.ts
+++ src/debug.ts
@@ -17,22 +17,22 @@ export const logDebugOutput = (originalDebits: Transaction[], originalCredits: T
   console.log('############ IGNORED CREDITS ###################')
   const ignoredCredits = originalCredits.filter(i => i.category === IGNORE)
   console.log(ignoredCredits.map(getFields))
-  fs.writeFileSync(debugOutputFilePath('credits.ignored.json'), JSON.stringify(ignoredCredits, null, 2))
+  fs.writeFileSync(debugOutputFilePath('credits.ignored'), JSON.stringify(ignoredCredits, null, 2))
 
   console.log('############ IGNORED DEBITS ###################')
   const ignoredDebits = originalDebits.filter(i => i.category === IGNORE)
   console.log(ignoredDebits.map(getFields))
-  fs.writeFileSync(debugOutputFilePath('debits.ignored.json'), JSON.stringify(ignoredDebits, null, 2))
+  fs.writeFileSync(debugOutputFilePath('debits.ignored'), JSON.stringify(ignoredDebits, null, 2))
 
   console.log('############ Uncategorizable CREDITS ###################')
   const uncategorizableCredits = credits.filter(i => !i.category)
   console.log(uncategorizableCredits.map(getFields))
-  fs.writeFileSync(debugOutputFilePath('credits.uncategorizable.json'), JSON.stringify(uncategorizableCredits, null, 2))
+  fs.writeFileSync(debugOutputFilePath('credits.uncategorizable'), JSON.stringify(uncategorizableCredits, null, 2))
 
   console.log('############ Uncategorizable DEBITS ###################')
   const uncategorizableDebits = debits.filter(i => !i.category)
   console.log(uncategorizableDebits.map(getFields))
-  fs.writeFileSync(debugOutputFilePath('debits.uncategorizable.json'), JSON.stringify(uncategorizableDebits, null, 2))
+  fs.writeFileSync(debugOutputFilePath('debits.uncategorizable'), JSON.stringify(uncategorizableDebits, null, 2))
 }
 
 
--- src/summary.ts
+++ src/summary.ts
@@ -1,35 +1,7 @@
 import { COMMA, IGNORE, TRANSACTION_TYPES, isTest } from './constants';
-import { CostaRica } from './descriptions/costaRica062024';
+import { costaRica062024 } from './categories/vacations.json';
 import { Transaction } from './transaction';
-
-const Amusements = "prime video channels, steam games, MORTONARB, Audible, AMAZON PRIME, TONYA RAE"
-const Auto = "thorntons, Reber, shell oil, COSTCO GAS, o'reilly, ILLINOISSECRETARYOFSTATE, IL TOLLWAY-AUTOREPLENISH, BP#, uber trip, ILSOS"
-const Education = "udemy, codecademy, WWW.KHANACADEMY.ORG, THRIFT BOOKS, COLLEGEBOARD"
-const Emergency = "onlyemergencyonetimes"
-const Charity = "ACTBLUE, WBEZ Chi Pub Media, NOILFOODBANK, wikipedia,girl scouts"
-const Check = "check"
-const Clothing = "zappos, JJ CLEANERS, nike, OLD NAVY, GAP"
-const EatingOut = "burger local, alexanders cafe, maple leaf coffee, burrito parrilla, deli, chick-fil-a, all chocolate kitchen, 7-eleven, dd/br, los cantaritos, loscantaritos, taqueria el sazon, mcdonald, starbucks, egg harbor, panera, indian harvest, panda express, first watch, penrose brewing, copper fox, duck donuts, DUNKIN, OBERWEIS, french 75, TELUGURUCHULU, ART HISTORY BREWING, geneva ale house, irish pub, thai corner cuisine, graham's, BATAVIA CREAMERY, DEAR DONUTS, SWEETSTOP, taylor st. pizza, OBSCURITY BREWING, SCHMIDTS TOWNE TAP, BATAVIA SMOKE N LIQUOR, THE SOUTHERN CAFE, ETHIO BEANS, MANCHU WOK, CHEZ GASTON, GROVE ORD, TIE ORDER, JAMBA, Hardware Restaurant, WENDY'S"
-const Fees = "safe deposit"
-const Gift = "paramount arts center, party city, sarkis pastry"
-const HomeImprovement = "menards, jc licht, sp igworks, lowes, home depot, at home store, benjamin moore, SAMPLIZE, STENCILREVOLUTION, thrift _and_ dollar, sherwin williams, toto fabrics, vikingsewinggallery, VIKINGSEWINGGALLERY657, DECORATIVE FABRICS DIRECT, HEINZ BROTHERS, HOMEDEPOT.COM, HARBOR FREIGHT"
-const HomeMaintenance = "Home Maintenance"
-const Kid = "st charles park district, music_and_arts.com, sounds like music, genevapark, county of kane school, PTO, VSI GENEVAPARKDISTIB, GENEVAPARKDISTWEB"
-const LawyerFees = "Lawyer Fees"
-const Medical = "THERASPECS, AMERICA's BEST, REMOTE ONLINE DEPOSIT, khanna, caremark, alliance clinical,riverview counseling, prose orthodontics, lawrence l johnson, cvs/pharmacy, atkinsons _and_ associates, northwestern my chart, PERIODONTAL MEDICINE PR, IN STEP BEHAVIORAL, JOHNSON DERMATOLOGY"
-const MetaCanada = "Meta Canada"
-const MetaStan = "Meta Stan"
-const MetaOneTime = "Meta Onetime"
-const MetaFood = "meijer, fresh thyme, THE FRESH MARKET, trader joe, aldi, jewel, COSTCO WHSE, caputo's, caputos, arbico organics"
-// ATM Withdrawal vs NON-CHASE ATM WITHDRAW captured by one string
-const Misc = "ATM Withdraw, ATM WITHDRAWAL, Zelle" 
-const Mortgage = "mortgage"
-const PersonalCare = "colour line, os2 salon, HARE_and_COMB, OMNILUX, ULTA"
-const Pet = "meadowview, petsmart, MEADOWVIEWVET.COM"
-const Shopping = "great estate, homegoods, AMZN Mktp, Amazon.com, etsy, joann stores, fabric wholesale, wal-mart, Michaels, blueland, goodwill retail, josh's frogs, BED BATH _AND_ BEYOND, target, walgreens, WORLD MARKET, WWW COSTCO COM, CASEYS, ONEHOMEBRAN, AMAZON MKTPL"
-const Utilities = "google one, patreon, netflix.com, chatgpt subscription, OPENAI, google storage, STATE FARM INSURANCE, disney plus, lakeshore recycling, metronet, ting, bitwarden, youtubepremium, lincoln natlife, Lincoln Nationa, nicor, geneva IL utility,GENEVA IL  UTILITY, Amazon web services, joanna pociecha, DelightRoom, Delight Room, garden my home, DAVEY TREE EXPERT COMPANY, PAYPAL  MICROSOFT, NordVPN" // delight room is alarmy
-const Vacation = `${CostaRica}, Vacation, AMERICAN, AIRBNB, Vrbo, EXPEDIA, FOREIGN TRANSACTION FEE, FOREIGN EXCHANGE RATE ADJUSTMENT FEE`
-
+import BaseCategories from './categories/base.json'
 /*
   WW CCD is reimbursement and should be zeroed with some purchase, so tagged for manual changes
   WW PPD is salary and FSAs and should be counted as income
@@ -38,35 +10,39 @@ const Vacation = `${CostaRica}, Vacation, AMERICAN, AIRBNB, Vrbo, EXPEDIA, FOREI
   College Savings ACH is a bank ACH_DEBIT and not a transfer-type, but ignored here because it is just moving money to college funds, not actually an expense that was incurred
 */
 const Ignore = "WW INTERNATIONAL PAYMENT CCD, College Savings ACH, peloton"
+const Vacation = `${costaRica062024}, ${BaseCategories.Vacation}`
 
 const categorySummary =
 {
-  [Amusements]: { umbrellaCategory: "Amusements", },
-  [Auto]: { umbrellaCategory: "Auto & Transport", },
-  [Education]: { umbrellaCategory: "Education", },
-  [Emergency]: { umbrellaCategory: "Emergency", },
-  [Charity]: { umbrellaCategory: "Charity", },
-  [Check]: { umbrellaCategory: "Check", },
-  [Clothing]: { umbrellaCategory: "Clothing", },
-  [EatingOut]: { umbrellaCategory: "Eating Out", },
-  [Fees]: { umbrellaCategory: "Fees", },
-  [Gift]: { umbrellaCategory: "Gift", },
-  [HomeImprovement]: { umbrellaCategory: "Home Improvement", },
-  [HomeMaintenance]: { umbrellaCategory: "Home Maintenance", },
-  [Kid]: { umbrellaCategory: "Kid", },
-  [LawyerFees]: { umbrellaCategory: "Lawyer Fees", },
-  [Medical]: { umbrellaCategory: "Medical", },
-  [MetaCanada]: { umbrellaCategory: "Meta Canada", },
-  [MetaStan]: { umbrellaCategory: "Meta Stan", },
-  [MetaOneTime]: { umbrellaCategory: "Meta OneTime", },
-  [MetaFood]: { umbrellaCategory: "Meta Food", },
-  [Misc]: { umbrellaCategory: "Misc", },
-  [Mortgage]: { umbrellaCategory: "Mortgage & Rent", },
-  [PersonalCare]: { umbrellaCategory: "Personal Care", },
-  [Pet]: { umbrellaCategory: "Pet", },
-  [Shopping]: { umbrellaCategory: "Shopping", },
-  [Utilities]: { umbrellaCategory: "Utilities", },
+  [BaseCategories.Amusements]: { umbrellaCategory: "Amusements", },
+  [BaseCategories.Auto]: { umbrellaCategory: "Auto & Transport", },
+  [BaseCategories.Education]: { umbrellaCategory: "Education", },
+  [BaseCategories.Emergency]: { umbrellaCategory: "Emergency", },
+  [BaseCategories.Charity]: { umbrellaCategory: "Charity", },
+  [BaseCategories.Check]: { umbrellaCategory: "Check", },
+  [BaseCategories.Clothing]: { umbrellaCategory: "Clothing", },
+  [BaseCategories.EatingOut]: { umbrellaCategory: "Eating Out", },
+  [BaseCategories.Fees]: { umbrellaCategory: "Fees", },
+  [BaseCategories.Gift]: { umbrellaCategory: "Gift", },
+  [BaseCategories.HomeImprovement]: { umbrellaCategory: "Home Improvement", },
+  [BaseCategories.HomeMaintenance]: { umbrellaCategory: "Home Maintenance", },
+  [BaseCategories.Kid]: { umbrellaCategory: "Kid", },
+  [BaseCategories.LawyerFees]: { umbrellaCategory: "Lawyer Fees", },
+  [BaseCategories.Medical]: { umbrellaCategory: "Medical", },
+  [BaseCategories.MetaCanada]: { umbrellaCategory: "Meta Canada", },
+  [BaseCategories.MetaStan]: { umbrellaCategory: "Meta Stan", },
+  [BaseCategories.MetaOneTime]: { umbrellaCategory: "Meta OneTime", },
+  [BaseCategories.MetaFood]: { umbrellaCategory: "Meta Food", },
+  // ATM Withdrawal vs NON-CHASE ATM WITHDRAW captured by one string
+  [BaseCategories.Misc]: { umbrellaCategory: "Misc", },
+  [BaseCategories.Mortgage]: { umbrellaCategory: "Mortgage & Rent", },
+  [BaseCategories.PersonalCare]: { umbrellaCategory: "Personal Care", },
+  [BaseCategories.Pet]: { umbrellaCategory: "Pet", },
+  [BaseCategories.Shopping]: { umbrellaCategory: "Shopping", },
+  [BaseCategories.Utilities]: { umbrellaCategory: "Utilities", },
+  
   [Vacation]: { umbrellaCategory: "Vacation", },
+
   [Ignore]: { umbrellaCategory: IGNORE, }
 }
 
